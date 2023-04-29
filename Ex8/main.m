a6604410725e7d077373c39d8f7733b682e19409
faggl <m.4esfd@gmail.com>
2023-04-29 19:09:18 +0200

    Hoffentlich fertig

--- Ex8/main.m
+++ Ex8/main.m
@@ -7,45 +7,53 @@ tol = 1.0d-6;
 eta = 0.9;
 gamma = 1.0d-4;
 
-
-% alpha = 20;
-% 
-% m= 20;
-% 
-% 
-% rng(300)
-% x0 = sqrt(20)*randn([n, 1]);
-% f = @(x) extRosenbrock(x, alpha);
-% gradf = @(x) extRosenbrockGradient(x, alpha);
-% X = L_BFGS(f,gradf,x0,m,tol,maxiter);
-
 alpha = [1, 20];
 n = [100, 1000, 10000];
 m = [1, 5, 20];
 
 results = zeros(5,1);
 
-tic
 for i = 1:2
     for j = 1:3
         for h = 1:3
-            tic
+
+            f     = augmentWithEvalCounter(@(x) extRosenbrock(x, alpha(i)));
+            gradf = augmentWithEvalCounter(@(x) extRosenbrockGradient(x, alpha(i)));
+            
+            % Reset counter
+            f('reset'); 
+            gradf('reset');
+
+            % Start of calculations
+            tic;
             rng(300)
             x0 = sqrt(20)*randn([n(j), 1]);
-            f = @(x) extRosenbrock(x, alpha(i));
-            gradf = @(x) extRosenbrockGradient(x, alpha(i));
+            %f = @(x) extRosenbrock(x, alpha(i));
+            %gradf = @(x) extRosenbrockGradient(x, alpha(i));
             X = L_BFGS(f,gradf,x0,m(h),tol,maxiter);
             time = toc;
-            num_fun_eval = 0;
             num_it = length(X(1,:))-1;
-            
+            num_f_eval = f('count'); 
+            num_gradf_eval = gradf('count');
+
             % Abspeichern Parameter und Ergebnisse
+
             results(1,end) = alpha(i);
             results(2,end) = n(j);
             results(3,end) = m(h);
-            results(4,end) = num_fun_eval;
+            results(4,end) = num_f_eval;
             results(5,end) = time;
             results(:,end+1) = zeros(5,1);
+
+
+            % Printout
+            fprintf('Parameters:   dim=%5d   -   alpha=%2d   -   m=%2d', n(j), alpha(i), m(h));
+            fprintf('\n')
+
+            fprintf(['Optimization ended: iterations=%d   -   evaluations f=%d   -   evaluations gradf=%d  ...' ...
+                '  -   runtime=%d'], num_it, num_f_eval, num_gradf_eval, time)
+            fprintf('\n \n')
+
         end
     end
 end
